9a47a9ebdaf84dad601cdfb455d4ee8f6cb92e49
Huynh_Ba_Anh <huynhbaanh03112003@gmail.com>
2025-10-03 09:27:19 +0700

    Fix CORS

--- app.js
+++ app.js
@@ -37,8 +37,6 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
